bb8aa236d9fa8fb5a5f3e3dbe9d824dad3def0b3
Your Name <you@example.com>
2021-01-13 16:01:09 -0300

    Criação de proJeto Mybills

---
+++ src/views/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home(){
+return(
+<div className="jumbotron">
+<h1 className="display-3">Bem vindo </h1>
+<p className="lead">Cadastre sua Conta aqui </p>
+<hr className="my-4"/>
+<p className="lead">
+<a className="btn btn-primary btn-lg" href="#" role="button">Cadastrar</a>
+</p>
+</div>
+)
+}
